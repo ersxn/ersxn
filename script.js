1a2f28de3d90b075f60846178f6085fac1de2c17
ersxn <ersyild@gmail.com>
2017-01-15 22:42:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$('#contact').click(function() {
+  $('div').html('emailersxnux@gmail.com');
+});
+
+$('#home').click(function() {
+  $('div').html('home');
+});
+
+$('#instagram').click(function() {
+  $('div').html('Aardvark');
+});
+
+$('#facebook').click(function() {
+  $('div').html('Aardvark');
+});
+
+$('#aardvark').click(function() {
+  $('div').html('Aardvark');
+});
